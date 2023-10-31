e17498e18f89bab563ffca0f5dd1d7efd8dd0b9a
lazargas <82470472+lazargas@users.noreply.github.com>
2023-10-31 10:50:56 +0530

    Ready for Production

--- src/build.sh
+++ src/build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 #exit on error
 set -o errexit
+cd..
 python3 -m venv venv
 source venv/bin/activate
 venv/bin/python -m pip install pip pip-tools rav --upgrade
 venv/bin/rav run installs
 rav run freeze
+cd src
 python manage.py collectstatic
 python manage.py migrate
