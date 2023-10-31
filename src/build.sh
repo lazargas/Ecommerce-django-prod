eeab1d70212dd5f5a12b315ad72fc68499f4580d
lazargas <82470472+lazargas@users.noreply.github.com>
2023-10-31 12:00:06 +0530

    Ready for Production

--- src/build.sh
+++ src/build.sh
@@ -3,13 +3,11 @@
 set -o errexit
 cd ..
 python --version
-python3 -m venv venv
-source venv/bin/activate
-venv/bin/python -m pip install pip pip-tools rav --upgrade
-venv/bin/pip-compile src/requirements/requirements.in -o src/requirements.txt
-venv/bin/python -m pip install -r src/requirements.txt
+pip install pip pip-tools rav --upgrade
+src/requirements/requirements.in -o src/requirements.txt
+pip install -r src/requirements.txt
 npm install
-venv/bin/python -m pip freeze
+-m pip freeze
 cd src
 python manage.py collectstatic --noinput
 python manage.py migrate
