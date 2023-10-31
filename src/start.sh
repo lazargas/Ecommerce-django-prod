9f2274595c8305dc20b69326efcb5befefbe5367
lazargas <82470472+lazargas@users.noreply.github.com>
2023-10-31 12:14:39 +0530

    Ready for Production

--- src/start.sh
+++ src/start.sh
@@ -2,5 +2,5 @@ cd ..
 python --version
 python3 -m venv venv
 source venv/bin/activate
-venv/bin/python cd src
-venv/bin/python waitress-serve --listen=*:8000 cfehome.wsgi:application
+cd src
+waitress-serve --listen=*:8000 cfehome.wsgi:application
